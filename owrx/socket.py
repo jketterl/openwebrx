e5e95e2c1a602a31a969676576e0cf87479dd71c
Fabian Franz <franz.fabian.94@gmail.com>
2022-07-27 23:09:45 +0200

    revert change in socket.py - does not need AF_INET6

--- owrx/socket.py
+++ owrx/socket.py
@@ -2,7 +2,7 @@ import socket
 
 
 def getAvailablePort():
-    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("", 0))
     s.listen(1)
     port = s.getsockname()[1]
