11a5e9f53c15633b07babaac2709ba35502d9739
Marat Fayzullin <luarvique@gmail.com>
2022-08-08 22:30:35 -0400

    Removed command line argument spacer, preventing negative values (-P -5) from parsing.

--- owrx/command.py
+++ owrx/command.py
@@ -57,7 +57,7 @@ class Flag(CommandMapping):
 class Option(CommandMapping):
     def __init__(self, option):
         self.option = option
-        self.spacer = " "
+        self.spacer = ""
 
     def map(self, value):
         if value is not None:
